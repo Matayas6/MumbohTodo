504688c0e7fb3d0a6e50e4a0ee284af673b418c0
Mathias <38738031+Matayas6@users.noreply.github.com>
2021-04-23 23:03:05 -0400

    Service Worker

--- MumbohTodo/wwwroot/js/site.js
+++ MumbohTodo/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+if ('caches' in window) {
+    const cacheName = 'offlineSave-V1';
+    let offlineBtn = $("#OfflineSave");
+
+    let checkCache = (url, changeCache = true) => {
+        caches.open(cacheName).then((cache) => {
+            cache.match(url).then((response) => {
+                let match = ('object' == typeof response);
+                if (match) {
+                    if (changeCache)
+                        cache.delete(window.location.href);
+
+                    if (changeCache) {
+                        offlineBtn.val('Save Offline');
+
+                    } else {
+                        offlineBtn.val('Remove');
+                    }
+                } else {
+                    if (changeCache)
+                        cache.add(window.location.href)
+
+                    if (changeCache) {
+                        offlineBtn.val('Remove')
+                    } else {
+                        offlineBtn.val('Save for offline');
+                    }
+                }
+            });
+        });
+
+    }
+        offlineBtn.on("click", () => {
+            checkCache(window.location.href);
+
+        });
+        checkCache(window.location.href, false);
+        offlineBtn.removeAttr('hiden');
+
+    }
---
+++ MumbohTodo/wwwroot/sw.js
@@ -0,0 +1,39 @@
+﻿const cacheName = 'cache-V1';
+const dynamicCache = 'dyn-cache-V1';
+let urlsToCache = [
+    '/ ',
+    '/css/site.css'
+];
+
+self.addEventListener('install', function (event) {
+    //    // Perform install steps
+    console.info('Service Worker Installed....');
+
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    console.info('Caching Started...');
+
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                const netResponse = fetch(event.request);
+                caches.open(dynamicCache).then((cache) => {
+                    cache.add(event.request);
+                });
+                return netResponse;
+            }
+            )
+    );
+});
